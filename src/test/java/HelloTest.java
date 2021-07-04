bb950bb5c26b08b21a2ba06a173d6baeafafe893
Dumitru-Catalin Clinca <cdccata@gmail.com>
2021-07-04 22:33:05 +0100

    Added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello test");
         System.out.println("Developing hello features");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 }
